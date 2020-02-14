bb3cce9b5774a26ff08e1f7417361918fa6f1c89
klusjd5 <60277489+klusjd5@users.noreply.github.com>
2020-02-13 21:15:39 -0700

    Update lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -3,13 +3,18 @@
 # Date: 2/13/2019
 
 #Problem 1 Code:
-#Make sure to document how you are solving each problem
+
+#having the user enter there name and a regular expression and then putting them into variables
 read -p "Enter a file name: " FILE
 read -p "Enter a regular expression: " REG
-
+#then using the regular expression and getting any line with it
 grep $REG $FILE
+#getting a count of any line that start with 3 integers and a -.
 grep -c ^[0-9][0-9][0-9]- regex_practice.txt
+#getting a count all the emails which start with @geocities.com
 grep -c @geocities.com regex_practice.txt
+#printing all the lines that start with 303-
 grep -n ^303- regex_practice.txt
+#moving the output of all the emails and putting it into a file
 grep -n "geocities.com" regex_practice.txt >> email_results.txt
  
